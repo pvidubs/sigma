01f1f93fb29a952ac0d113973da9adf3d9b90930
PVIdubs <nluismi80@gmail.com>
2023-02-24 11:20:27 -0500

    working on nav-bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,46 @@
 <script setup>
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import UserCta from './UserCta.vue';
+import SigmaIcon from './icons/SigmaIcon.vue';
+
+const closeIconHidden = ref(true);
+const navBarColor = ref('bg-sigma');
+const iconColor = ref('fill-white');
+
+const menuInteraction = () => {
+    if (closeIconHidden.value) {
+        navBarColor.value = 'bg-white';
+        iconColor.value = 'fill-sigma';
+    } else {
+        navBarColor.value = 'bg-sigma';
+        iconColor.value = 'fill-white';
+    }
+    closeIconHidden.value = !closeIconHidden.value;
+};
 </script>
 
 <template>
-    <div class="bg-sigma flex justify-between items-center p-3 lg:py-4 lg:px-6 font-poppins">
+    <div
+        :class="`${navBarColor} flex justify-between items-center p-4 lg:py-4 lg:px-6 font-poppins`"
+    >
         <RouterLink to="/">
-            <img src="../assets/icons/logo1.svg" class="w-24 lg:w-32" />
+            <SigmaIcon :class="`w-28 lg:w-32 ${iconColor}`" />
         </RouterLink>
-        <img src="../assets/icons/hamburger-menu.svg" width="20" class="block lg:hidden" />
+        <img
+            :src="`../src/assets/icons/hamburger-menu.svg`"
+            class="block lg:hidden"
+            width="28"
+            @click="menuInteraction"
+            v-if="closeIconHidden"
+        />
+        <img
+            :src="`../src/assets/icons/close-menu.svg`"
+            class="block lg:hidden relative right-0.5"
+            width="22"
+            @click="menuInteraction"
+            v-if="!closeIconHidden"
+        />
         <div class="space-x-7 hidden lg:flex">
             <RouterLink to="/buy" class="text-white">Comprar</RouterLink>
             <RouterLink to="/rent" class="text-white">Rentar</RouterLink>
