e8fdfa4b54a5065de928cb489ce9cb94d6df5842
PVIdubs <nluismi80@gmail.com>
2023-03-06 16:56:00 -0500

    working on footer section

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import NavBar from './components/NavBar.vue';
-import FooterSection from './components/FooterSection.vue';
+import { RouterLink, RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+import FooterSection from "./components/FooterSection.vue";
 </script>
 
 <template>
-    <header>
-        <NavBar />
-    </header>
-    <main>
-        <RouterView />
-    </main>
-    <footer>
-        <FooterSection />
-    </footer>
+  <header>
+    <NavBar />
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <FooterSection />
+  </footer>
 </template>
 
